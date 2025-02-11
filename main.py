811bdcecafc4adf84eee480dc007d8e15edab401
SquirrelRex <leftseat12@pm.me>
2025-02-11 00:22:17 -0700

    changed basically everything you could dream of

--- main.py
+++ main.py
@@ -2,7 +2,33 @@ def main ():
 
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+    #print(file_contents)
+    #print(count_words(file_contents))
+    #print(count_characters(file_contents))
+    print_letters(count_characters(file_contents))
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_characters(text):
+    lower_text = text.lower()
+    character_count = {}
+    for character in lower_text:
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
+    return character_count
+
+def print_letters(character_dict):
+    for character in character_dict:
+        if character.isalpha() == True:
+            print(f"The '{character}' character was found {character_dict[character]} times")
+
+
 
 if __name__ == "__main__":
     main()
